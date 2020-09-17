387ea240df5905943f39e66fa7465799606c0a5c
Fabian Hendriks <19959@ma-web.nl>
2020-09-17 09:43:45 +0200

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,35 @@
+<?php ?>
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/template.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="navigation">
+    <div class="navigation-content">
+        <div class="navigation-button">
+            <a href="" class="navigation-link">OVER ONS</a>
+        </div>
+        <div class="navigation-button">
+            <a href="" class="navigation-link">DIENSTEN</a>
+        </div>
+        <div class="navigation-logo">
+            <a href="">
+                <img src="images/Luso%20-%20DEF%20-%20RGB-02.svg" alt="Logo" class="navigation-logo-image">
+            </a>
+        </div>
+        <div class="navigation-button">
+            <a href="" class="navigation-link">PORTFOLIO</a>
+        </div>
+        <div class="navigation-button">
+            <a href="" class="navigation-link">CONTACT</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
