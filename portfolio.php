624550039cdd123d7b398f588fa785f66f57219b
Fabian Hendriks <19959@ma-web.nl>
2020-09-17 15:03:43 +0200

    goede css gekoppelt en page content toegevoegd

--- portfolio.php
+++ portfolio.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style/template.css">
+    <link rel="stylesheet" href="style/portfolio.css">
     <title>Document</title>
 </head>
 <body>
@@ -30,6 +30,42 @@
             <a href="" class="navigation-link">CONTACT</a>
         </div>
     </div>
+</div>
+<div class="page-content">
+    <img src="https://via.placeholder.com/359x166?text=Placeholder" alt="" class="big-picture">
+    <div class="work-container">
+        <div class="work-item">
+            <h2 class="work-item-head">Voorbeeldtitel</h2>
+            <div class="work-item-text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum."
+            </div>
+
+            <div class="work-item">
+                <h2 class="work-item-head">Voorbeeldtitel</h2>
+                <div class="work-item-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                    qui officia deserunt mollit anim id est laborum."</div>
+            </div>
+
+            <div class="work-item">
+                <h2 class="work-item-head">Voorbeeldtitel</h2>
+                <div class="work-item-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                    qui officia deserunt mollit anim id est laborum."</div>
+            </div>
+        </div>
+    </div>
+
+
+
 </div>
 </body>
 </html>
