02fe0279aefe0e4821ecc1abfc1ff7a9de263f85
Fabian <19959@ma-web.nl>
2020-09-20 22:49:07 +0200

    script aangepast

--- script/stop-loop.js
+++ script/stop-loop.js
@@ -1,6 +1,6 @@
 function stopLoop(){
     let image = document.getElementById('loop');
-    if (image.src.match("Looping")){
+    if (image.src.match("doorlopen")){
         image.src = "images/Luso - DEF - RGB-02.svg";
     }
 }
