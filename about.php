43ef5b719ae4d209a2ec3c978e25dcd9a3ac1a26
Fabian <19959@ma-web.nl>
2020-09-21 15:58:33 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,54 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/about.css">
+    <script src="script/stop-loop.js"></script>
+    <title>Diensten</title>
+</head>
+<body>
+<div class="navigation">
+    <div class="navigation-content">
+        <div class="navigation-button">
+            <a href="about.php" class="navigation-link link-active">OVER ONS</a>
+        </div>
+        <div class="navigation-button">
+            <a href="products.php" class="navigation-link">DIENSTEN</a>
+        </div>
+        <div class="navigation-logo">
+            <a href="">
+                <img src="images/doorlopen.gif" alt="Logo" class="navigation-logo-image" id="loop"
+                     onload="setTimeout(stopLoop, 1000)">
+            </a>
+        </div>
+        <div class="navigation-button">
+            <a href="portfolio.php" class="navigation-link">PORTFOLIO</a>
+        </div>
+        <div class="navigation-button">
+            <a href="" class="navigation-link">CONTACT</a>
+        </div>
+    </div>
+</div>
+<div class="page-content">
+    <div class="image-holder">
+        <img src="https://via.placeholder.com/359x166?text=Placeholder" alt="" class="big-picture">
+    </div>
+    <div class="about-us">
+        <h1 class="about-us__head">Over ons</h1>
+        <p class="about-us__text">Dit is ons team, gemotiveerd om u een product te geven waar u het helemaal mee eens
+            bent. Dit is onze specialiteit aangezien we het erg belangrijk vinden om u (de klant) mee te nemen in het
+            ontwerp proces. Het team deelt deze mindset om zo veel mogelijk contact te behouden en iets te leveren waar
+            u zelf deel van hebt uitgemaakt. Het team staat altijd klaar en staat open voor communicatie.</p>
+        <div class="about-us__underline"></div>
+    </div>
+</div>
+</body>
+</html>
+
+
